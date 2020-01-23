ae134d918096128c6e0c57c3332e62e066b3810b
ram4kSTT <hfvbkm24102006>
2020-01-23 11:00:34 +0500

    Update main.py
    apload new fitches loging, modernizing def pazvorot and def turn

--- kladovshik/main.py
+++ kladovshik/main.py
@@ -17,27 +17,19 @@ grabMotor = Motor(Port.D)
 colorSensorGruz = ColorSensor(Port.S1)
 colorSensorRight = ColorSensor(Port.S2)
 colorSensorLeft = ColorSensor(Port.S3)
-directionRotation = 1
-directionGrab = 1
-Const_turn_Angle = 400
-Const_turn_Speed_Left = 50
-Const_turn_Speed_Right = -50
-Const_Razvorot_Angle = 180
-Const_Razvorot_Speed_Left = 200
-Const_Razvorot_Speed_Right = 200
+Const_turn_Angle = 155
+Const_turn_Speed = 50
+Const_Razvorot_Angle = 330
+Const_Razvorot_Speed = 50
 Const_Grab_Speed = 200
 Const_Grab_angle = 150
-Const_Go_Speed_Left = 200
-Const_Go_Speed_Right = 200
+Const_Go_Speed = 200
 Const_Relection_Limit = 20
 Const_Speed_Fline = 20
 Const_Slow_Speed_Fline = 50
 Const_MotorRule_Speed = 150
-directionTurn = 1
-xRobot = 2
-yRobot = 0
-xPlatform = 0
-yPlatform = 0
+Robot = [2,0]
+Platform = [0,0]
 
 ##################################################################################
 ColorMap = [
@@ -45,137 +37,149 @@ ColorMap = [
     [Color.GREEN,Color.RED,Color.BLUE]
     ]
 #####################################################################################
-
+#разварачиваемся на 90 градусов
+#directionTurn - выбор направления поворота  значение может быть 1 (направо) или -1 (налево)
 def turn(directionTurn):
-    leftMotor.reset_angle(0)
-    leftMotor.dc(Const_turn_Speed_Left * directionRotation* directionTurn)
-    rightMotor.dc(Const_turn_Speed_Right * directionRotation* directionTurn)
-    while(True):
-        angle = leftMotor.angle()
-        if(angle == Const_turn_Angle):
-            leftMotor.stop()
-            rightMotor.stop()
-            break
-def razovorot():
-    leftMotor.reset_angle(0)
-    leftMotor.dc(Const_turn_Speed_Left * directionRotation * directionRazvorot)
-    rightMotor.dc(Const_turn_Speed_Right * directionRotation * directionRazvorot)
-    while(True):
-        angle = leftMotor.angle()
-        if(angle == Const_Razvorot_Angle):
-            leftMotor.stop()
-            rightMotor.stop()
-            break
-def grab():
-  grabMotor.reset_angle(0)
-  grabMotor.dc(Const_Grab_Speed * directionGrab)
+  print("turn:")
+  print(directionTurn)
+  leftMotor.reset_angle(0)
+  leftMotor.dc(Const_turn_Speed *  directionTurn)
+  rightMotor.dc(-Const_turn_Speed *  directionTurn)
   while(True):
-        grabMotor.angle()
-        angleGrab = grabMotor.angle()
-        if(angleGrab == Const_Grab_angle ):
-            grabMotor.stop()
-            break
+      angle = abs(leftMotor.angle())
+      if(angle >= Const_turn_Angle):
+          leftMotor.stop()
+          rightMotor.stop()
+          break
+#разварачиваемся на 180 градусов
+def razovorot():
+  print("разворачиваемся")
+  leftMotor.reset_angle(0)
+  leftMotor.dc(Const_turn_Speed)
+  rightMotor.dc(-Const_turn_Speed)
+  while(True):
+      angle = abs(leftMotor.angle())
+      if(angle >= Const_Razvorot_Angle):
+          leftMotor.stop()
+          rightMotor.stop()
+          break
+          
+
+
+#захват кубика манипулятором
+def grab():
+  print("захватываем кубик")
+  grabMotor.reset_angle(0)
+  grabMotor.dc(Const_Grab_Speed)
+  while(True):
+    grabMotor.angle()
+    angleGrab = grabMotor.angle()
+    if(angleGrab >= Const_Grab_angle ):
+        print("кубик")
+        grabMotor.stop()
+        return
+
+
 
 #езда по черной линии
-#crossroadCounts - кол-во перекрестков, которые необходимо проехать
+#left - мощность левого мотора
+#right - мощность правого мотора
 def motorRule(left,right):
-    leftMotor.run(Const_MotorRule_Speed * directionRotation)
-    rightMotor.run(Const_MotorRule_Speed * directionRotation)
+    leftMotor.run(left)
+    rightMotor.run(right)
+
+
 
 #езда по черной линии
 #crossroadCounts - кол-во перекрестков, которые необходимо проехать
 def fLine(crossroadCounts):
-    colors = [Color.GREEN,Color.RED,Color.BLUE,Color.BROWN]
-    colorCounts = 0
-    crossroad = 0
-    blackLine = False
-    while(True):
-      reflectionLEFT = colorSensorLeft.reflection()
-      reflectionRight = colorSensorRight.reflection()
+  print("fLine:")
+  print(crossroadCounts)
 
-      if(reflectionLEFT < Const_Relection_Limit):
-        if(reflectionRight < Const_Relection_Limit):
-          motorRule(Const_Speed_Fline,Const_Speed_Fline)
-        else:
-          motorRule(-Const_Slow_Speed_Fline,Const_Slow_Speed_Fline)
+  colors = [Color.GREEN,Color.RED,Color.BLUE,Color.BROWN]
+  colorCounts = 0
+  crossroad = 0
+  blackLine = False
+  while(True):
+    reflectionLEFT = colorSensorLeft.reflection()
+    reflectionRight = colorSensorRight.reflection()
+
+    if(reflectionLEFT < Const_Relection_Limit):
+      if(reflectionRight < Const_Relection_Limit):
+        motorRule(Const_Speed_Fline,Const_Speed_Fline)
       else:
-        if(reflectionRight < Const_Relection_Limit):
-          motorRule(Const_Slow_Speed_Fline,-Const_Slow_Speed_Fline)
-        else:
-          motorRule(Const_Speed_Fline,Const_Speed_Fline)
-      
-      if((reflectionLEFT < Const_Relection_Limit) & (reflectionRight < Const_Relection_Limit)):
-        if(blackLine == False):
-          crossroad = crossroad + 1
-          if(crossroad == crossroadCounts):
-            motorRule(0,0)
-            break
-        blackLine = True
+        motorRule(-Const_Slow_Speed_Fline,Const_Slow_Speed_Fline)
+    else:
+      if(reflectionRight < Const_Relection_Limit):
+        motorRule(Const_Slow_Speed_Fline,-Const_Slow_Speed_Fline)
       else:
-        blackLine = False
-      color = colorSensorGruz.color()
-      if(color in colors):
-        print(color)
-        colorCounts = colorCounts + 1
-        if(colorCounts > 0):
+        motorRule(Const_Speed_Fline,Const_Speed_Fline)
+    
+    if((reflectionLEFT < Const_Relection_Limit) & (reflectionRight < Const_Relection_Limit)):
+      if(blackLine == False):
+        crossroad = crossroad + 1
+        if(crossroad == crossroadCounts):
           motorRule(0,0)
           break
-
-
-
+      blackLine = True
+    else:
+      blackLine = False
+    color = colorSensorGruz.color()
+    if(color in colors):
+      print(color)
+      colorCounts = colorCounts + 1
+      if(colorCounts > 0):
+        motorRule(0,0)
+        break
+#определяет путь от местоположение робота до нужной платформы
 def logic():
-  povorot = 0
-  countsBlackCrossroad =abs(xRobot - xPlatform)
+  print("logic:")
+  print("рассчитываем путь")
+  povorot2 = 0
+  povorot1 = 1
+  countsBlackCrossroad =abs(Robot[0] - Platform[0])
   robotWay = [["grab", 1], ["razovorot", 0 ],["fLine",1] ]
-  if(yRobot == 0):
-    directionRotation * -1 
-  if(xRobot - xPlatform  == -1) or (xRobot - xPlatform  == -2):
-   povorot = -1
+  
+  if(Robot[1] == 0):
+    povorot1 * -1 
+  if(Robot[0] - Platform[0]  == -1) or (Robot[0] - Platform[0]  == -2):
+   povorot2 = -1
   else:
-    povorot = 1
-  
-  
-  if(xRobot - xPlatform  == -1) or (xRobot - xPlatform  == -2) and (yPlatform == 0):
-    povorot = -1
-    
-  elif(xRobot - xPlatform  == -1) or (xRobot - xPlatform  == -2) and (yPlatform == 1):
-    povorot = 1
-  
-  elif(xRobot - xPlatform  == 1) or (xRobot - xPlatform  == 2) and (yPlatform == 0):
-    povorot = -1
-   
-  elif(xRobot - xPlatform  == -1) or (xRobot - xPlatform  == -2) and (yPlatform == 1):
-    povorot = 1
-  elif(xRobot == xPlatform) and (yRobot == yPlatform):
+    povorot2 = 1
+  if(Robot[0] - Platform[0]  == -1) or (Robot[0] - Platform[0]  == -2) and (Platform[1] == 0):
+    povorot2 = -1
+  elif(Robot[0] - Platform[0]  == -1) or (Robot[0] - Platform[0]  == -2) and (Platform[1] == 1):
+    povorot2 = 1
+  elif(Robot[0] - Platform[0]  == 1) or (Robot[0] - Platform[0]  == 2) and (Platform[1] == 0):
+    povorot2 = -1
+  elif(Robot[0] - Platform[0]  == -1) or (Robot[0] - Platform[0]  == -2) and (Platform[1] == 1):
+    povorot2 = 1
+  elif(Robot[0] == Platform[0]) and (Robot[1] == Platform[1]):
     robotWay.append(["MotorRule",2])
     robotWay.append(["razovorot",0])
     robotWay.append(["fLine",1])
-  elif(xRobot == xPlatform):
+  elif(Robot[0] == Platform[0]):
     robotWay.append(["MotorRule",0])
-  
-  robotWay.append(["turn", povorot])
+  robotWay.append(["turn", povorot1])
   robotWay.append(["fLine",countsBlackCrossroad])
-  robotWay.append(["turn", povorot])
+  robotWay.append(["turn", povorot2])
   return robotWay
 
-
+#проезжает от местоположения робота до нужной платформы
+#robotWay - инструкция как доехать от местоположения робота до нужной платформы
 def perfomer(robotWay):
+  print("perfomer:")
+  print(robotWay)
   for way in robotWay:
     if(way[0] == "turn"):
       turn(way[1])
     elif(way[0] == "razovorot"):
-      razovorot(way[1])
+      razovorot()
     elif(way[0] == "fLine"):
       fLine(way[1])
     elif(way[0] == "grab"):
       grab()
 
 
+
 perfomer(logic())
-
-
-
-    
-
-
-
